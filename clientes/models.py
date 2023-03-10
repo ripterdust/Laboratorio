0a0325d56a85355531e24711bd1232df5dd4490a
ripterdust <bryantello2010@hotmail.com>
2023-03-10 15:34:18 -0600

    Migraciones

--- clientes/models.py
+++ clientes/models.py
@@ -1,6 +1,8 @@
 from django.db import models
-
+import datetime
 # Create your models here.
 class Clients(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, default='N/A', null=False)
+    birth = models.DateField(default=datetime.date.today)
+    dpi = models.CharField(max_length=20, null=False, default='N/A')
     created_at = models.DateField(auto_now=True)
