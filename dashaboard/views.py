4c5d22f64d4a3e38973d0f5462090326d67aaafc
ripterdust <bryantello2010@hotmail.com>
2023-03-10 17:27:05 -0600

    Agregado el total de laboratorios

--- dashaboard/views.py
+++ dashaboard/views.py
@@ -2,12 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from clientes.models import Clients
-
+from laboratories.models import Laboratory
 # Create your views here.
 @login_required
 def dashboard(request):
     clients = Clients.objects.all().count()
+    laboratories = Laboratory.objects.all().count()
 
-    context = {'total_clients': clients}
+
+    context = {'total_clients': clients, 'total_laboratories': laboratories}
 
     return render(request, 'home.html', context)
