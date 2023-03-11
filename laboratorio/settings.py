f4ac4b5fa177799026dabe93a9b47ef27917975d
ripterdust <bryantello2010@hotmail.com>
2023-03-10 22:37:01 -0600

    main, Agregado el modelo de los resultados del examen

--- laboratorio/settings.py
+++ laboratorio/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'dashaboard',
     'laboratories',
     'fields',
-    'testsApp'
+    'testsApp',
+    'testResultFields'
 ]
 
 STATICFILES_DIRS = [
--- testResultFields/models.py
+++ testResultFields/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+from testsApp.models import Test
 # Create your models here.
+class TestResultField(models.Model):
+    test = models.ForeignKey(Test, on_delete=models.PROTECT)
+    name = models.CharField(max_length=255, null=False)
+    result = models.CharField(max_length=255, null=False)
+    measurment = models.CharField(max_length=255, null=False)
