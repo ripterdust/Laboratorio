a05ebc773938be8df6d9f9350c0e6956a65c16de
ripterdust <bryantello2010@hotmail.com>
2023-03-10 20:11:10 -0600

    Added the testlaboratories

--- testsApp/models.py
+++ testsApp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from clientes.models import Clients
+from laboratories.models import Laboratory
 
 # Create your models here.
+class Test(models.Model):
+    patient = models.ForeignKey(Clients, on_delete=models.PROTECT)
+    lab = models.ForeignKey(Laboratory, on_delete=models.PROTECT)
