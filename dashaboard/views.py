553e37d5080b9a49844d38b4932bde96c9081d45
ripterdust <bryantello2010@hotmail.com>
2023-03-10 20:19:57 -0600

    main, Agregado el cuadro de los tests

--- dashaboard/views.py
+++ dashaboard/views.py
@@ -3,13 +3,15 @@ from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from clientes.models import Clients
 from laboratories.models import Laboratory
+from testsApp.models import Test
+
 # Create your views here.
 @login_required
 def dashboard(request):
     clients = Clients.objects.all().count()
     laboratories = Laboratory.objects.all().count()
+    tests = Test.objects.all().count()
 
-
-    context = {'total_clients': clients, 'total_laboratories': laboratories}
+    context = {'total_clients': clients, 'total_laboratories': laboratories, 'total_tests': tests}
 
     return render(request, 'home.html', context)
