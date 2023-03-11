506bf2856c5d72b967856a260008ac387a77d4d0
ripterdust <bryantello2010@hotmail.com>
2023-03-10 20:43:29 -0600

    main, Agregados los commits

--- testsApp/urls.py
+++ testsApp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import index
+from .views import index, fill_fields
 
 urlpatterns = [
-    path('', index, name="tests")
+    path('', index, name="tests"),
+    path('fill-fields/<int:test_id>', fill_fields, name='fill_fields')
 ]
--- testsApp/views.py
+++ testsApp/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render, redirect
 from .models import Test
 from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
 
 # Create your views here.
+@login_required
 def index(request):
 
-    tests = Test.objects.all()
+    tests = Test.objects.select_related('patient', 'lab')
     page = request.GET.get('page', 1)
 
     try:
@@ -18,3 +21,7 @@ def index(request):
 
 
     return render(request, 'tests.html', context)
+
+@login_required
+def fill_fields(request, test_id):
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/'))
