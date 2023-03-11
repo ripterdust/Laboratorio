0cfb2c9a03f6a183d41fad50db201533f3e459c4
ripterdust <bryantello2010@hotmail.com>
2023-03-10 23:04:30 -0600

    main, Agregado el completar un test

--- testResultFields/models.py
+++ testResultFields/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 from testsApp.models import Test
+from fields.models import Field
 # Create your models here.
 class TestResultField(models.Model):
     test = models.ForeignKey(Test, on_delete=models.PROTECT)
-    name = models.CharField(max_length=255, null=False)
-    result = models.CharField(max_length=255, null=False)
-    measurment = models.CharField(max_length=255, null=False)
+    field = models.ForeignKey(Field, on_delete=models.PROTECT)
+    result = models.CharField(max_length=255)
--- testsApp/views.py
+++ testsApp/views.py
@@ -4,6 +4,7 @@ from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from fields.models import Field
+from testResultFields.models import TestResultField
 
 # Create your views here.
 @login_required
@@ -26,6 +27,10 @@ def index(request):
 @login_required
 def fill_fields(request, test_id):
     test = Test.objects.select_related('patient', 'lab').get(id = test_id)
+
+    if test.completed:
+        return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/'))
+        
     fields = Field.objects.filter(laboratory=test.lab.id)
 
     context = {'test': test, 'fields': fields}
@@ -70,12 +75,29 @@ def uncompleted_tests(request):
 def save_uncompleted_test(request, test_id):
     post = request.POST
 
+    test = Test.objects.get(id = test_id)
+
+    if test.completed:
+        return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/'))
+
     fields_string = post.get('fields')
 
     fields_dict = eval(fields_string)
     fields_list = list(fields_dict.keys())
 
     for field in fields_list:
-        print(field)
+        data_field = Field.objects.get(id=int(field))
+        test_result_field = TestResultField()
+        
+        actual_field = fields_dict[field]
+        test_result_field.field = data_field
+        test_result_field.result = actual_field
+        test_result_field.test = test
+
+        test_result_field.save()
+
+    
+    test.completed = True
+    test.save()
 
     return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/'))
