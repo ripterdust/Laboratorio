5420cf34e0f2a5c7215c8ab935694ec3e2ca0383
ripterdust <bryantello2010@hotmail.com>
2023-03-10 21:22:25 -0600

    main, Movido el elemento de laboratorios

--- dashaboard/views.py
+++ dashaboard/views.py
@@ -4,14 +4,20 @@ from django.contrib.auth.decorators import login_required
 from clientes.models import Clients
 from laboratories.models import Laboratory
 from testsApp.models import Test
+from datetime import datetime, timedelta
+from django.db.models import Sum
 
 # Create your views here.
 @login_required
 def dashboard(request):
+
+    last_month = datetime.today() - timedelta(days=30)
     clients = Clients.objects.all().count()
     laboratories = Laboratory.objects.all().count()
-    tests = Test.objects.all().count()
+    tests = Test.objects.filter(created_at__gte=last_month)
+    test_count = tests.count()
+    total_sales = tests.select_related('lab').aggregate(sum=Sum('lab__price'))['sum']
 
-    context = {'total_clients': clients, 'total_laboratories': laboratories, 'total_tests': tests}
+    context = {'total_clients': clients, 'total_laboratories': laboratories, 'total_tests': test_count, 'total_sales': total_sales}
 
     return render(request, 'home.html', context)
