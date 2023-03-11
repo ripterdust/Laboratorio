d710dd41d2912e17e0a4add212c4dbb00fb3ef25
ripterdust <bryantello2010@hotmail.com>
2023-03-11 00:57:57 -0600

    main, Agregada la gráfica de las ventas del día

--- dashaboard/views.py
+++ dashaboard/views.py
@@ -4,8 +4,9 @@ from django.contrib.auth.decorators import login_required
 from clientes.models import Clients
 from laboratories.models import Laboratory
 from testsApp.models import Test
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, date
 from django.db.models import Sum
+from calendar import monthrange
 
 # Create your views here.
 @login_required
@@ -18,6 +19,30 @@ def dashboard(request):
     test_count = tests.count()
     total_sales = tests.select_related('lab').aggregate(sum=Sum('lab__price'))['sum']
 
-    context = {'total_clients': clients, 'total_laboratories': laboratories, 'total_tests': test_count, 'total_sales': total_sales}
+    sales_by_month = []
+    today = date.today()
+    year = today.year
+    month = today.month
+
+    for i in range(1, monthrange(year, month)[1] + 1):
+        test_month = Test.objects.filter(
+            created_at__gte=last_month, 
+            created_at__year__gte = year, 
+            created_at__day__gte = i, 
+            created_at__day__lte =i
+        )
+        sales = test_month.select_related('lab').aggregate(sum=Sum('lab__price'))['sum']
+
+        if not sales:
+            sales = 0
+
+        sales_by_month.append(sales)
+
+    context = {'total_clients': clients, 
+        'total_laboratories': laboratories, 
+        'total_tests': test_count, 
+        'total_sales': total_sales,
+        'data': sales_by_month
+    }
 
     return render(request, 'home.html', context)
