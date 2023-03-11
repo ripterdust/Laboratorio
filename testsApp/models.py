9cdf88a2e5ac906b78243a56375dee82c73de48c
ripterdust <bryantello2010@hotmail.com>
2023-03-10 20:16:32 -0600

    main, Agregadas las migraciones y los tests

--- testsApp/models.py
+++ testsApp/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 from clientes.models import Clients
 from laboratories.models import Laboratory
-
+import datetime
 # Create your models here.
 class Test(models.Model):
     patient = models.ForeignKey(Clients, on_delete=models.PROTECT)
     lab = models.ForeignKey(Laboratory, on_delete=models.PROTECT)
+    created_at = models.DateField(default=datetime.date.today)
