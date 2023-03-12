8ecf8aaf38d56da3ee57f18f0e927ed56083a4d5
ripterdust <bryantello2010@hotmail.com>
2023-03-11 18:59:32 -0600

    main, Arreglada la tabla

--- fields/models.py
+++ fields/models.py
@@ -4,4 +4,5 @@ from laboratories.models import Laboratory
 class Field(models.Model):
     name = models.CharField(max_length=255)
     measurment = models.CharField(max_length=255)
+    reference = models.CharField(max_length=255, default="No hay referencia")
     laboratory = models.ForeignKey(Laboratory, on_delete=models.CASCADE)
