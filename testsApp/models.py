b643c2ed5ff3b8060cab5fac9dfd3e4da8e0b9ae
ripterdust <bryantello2010@hotmail.com>
2023-03-11 18:46:27 -0600

    main, Agregados los comentarios0

--- testsApp/models.py
+++ testsApp/models.py
@@ -7,4 +7,5 @@ class Test(models.Model):
     patient = models.ForeignKey(Clients, on_delete=models.PROTECT)
     lab = models.ForeignKey(Laboratory, on_delete=models.PROTECT)
     completed = models.BooleanField(default=False)
+    comment = models.CharField(default='No hay comentarios', max_length=255)
     created_at = models.DateField(default=datetime.date.today)
